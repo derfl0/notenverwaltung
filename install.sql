5883dce0212b403b93d0eb61c54231ec6465814e
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-07-07 10:24:15 +0200

    Non blocking install

--- install.sql
+++ install.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE `notenverwaltung` (
+CREATE TABLE IF NOT EXISTS `notenverwaltung` (
   `user_id` varchar(32) COLLATE latin1_german1_ci NOT NULL,
   `seminar_id` varchar(32) COLLATE latin1_german1_ci NOT NULL,
   PRIMARY KEY (`user_id`,`seminar_id`)
